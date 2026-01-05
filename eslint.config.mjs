0d14a2fa53d0478f35d62a1480d0d5a5581ea520
Villani85 <servizi.villani@gmail.com>
2026-01-05 01:35:25 +0100

    fix(eslint): update flat config compat options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
 import { FlatCompat } from "@eslint/eslintrc"
 import { dirname } from "path"
 import { fileURLToPath } from "url"
-import nextConfig from "eslint-config-next"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: false,
+  allConfig: false,
 })
 
 export default [
